efc87fb1eaac015c257f37d40de902ec70316c20
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-07 21:16:58 -0500

    Add files via upload

---
+++ TD-BANNER.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+clear
+apt update -y
+apt upgrade -y
+apt install figlet -y
+echo "$azulcielo"
+figlet instalando
+  sleep 3.0
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+#PRIMER MENSAJE
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m\e[32m CREADOR :\e[33m @Thony_DroidYT _/<(°-•)>\_"
+   sleep 3.0
+#SEGUNDO MENSAJE
+clear
+echo ""
+echo ""
+echo ""
+echo -e "$amarillo   _/<(°-')>\_===> ThonyDroidYT <===_/<('-°)>\_"
+echo ""
+echo ""
+echo ""
+#COLORES
+rojo='\e[1m\e[31m'
+verde='\e[1m\e[32m'
+blanco='\e[1m\e[38m'
+amarillo='\e[1m\e[33m'
+azulcielo='\e[1m\e[36m'
+   sleep 3.0
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$verde |            Creado por: ThonyDroidYT          |"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$blanco |            Script By: THONYDROID           |"
+   sleep 0.5
+echo -e "$amarillo |            YOUTUBE: THONYDROID          |"
+   sleep 0.5
+echo -e "$blanco |----------------------------------------------|"
+   sleep 0.5
+echo -e "$azulcielo |        VIDA DE PROGRAMADOR [ T.D]          |"
+   sleep 0.5
+echo -e "$blanco |----------------------------------------------|"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$verde |      SCRIPT HECHO PARA MIS SEGUIDORES     |"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 3.0
+echo -e "$verde[ESPERA UN MOMENTO]"
+echo -e "$rojo[PARA APOYARME EN MI CANAL]"
+   sleep 15.0
+#canal de THONYDROID
+termux-open https://www.youtube.com/channel/UCCPVbngD25cA-SGlRpGNXng
+#clear
+#figlet -f big BANNER | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+#echo -e $lightgreen "\e[1mSI TIENES DUDAS COMUNICARME EN MIS REDES SOCIALES @Thony_DroidYT... "
+#echo " "
+#echo -e $yellow "\e[1mTD-BANNER CREADOR: \e[0;31mTHONYDROID... "
+#echo -e "\e[0;31m CANAL DE \e[0;32mYOU-TUBE:  \e[0;34mhttps://www.youtube.com/channel/UCCPVbngD25cA-SGlRpGNXng"
+#echo -e "\e[0;31mT\e[0;32mH\e[0;31mO\e[0;32mN\e[0;31mY\e[0;32mD\e[0;31mR \e[0;32mO\e[0;31mI\e[0;32mD"
+#echo -e $yellow "\e[1mCREDITOS: \e[0;31mHACKING PCH... "
+#echo " "
+#echo " "
+echo " "
+echo -e -n "\e[1m\e[33m\nCual es tu \e[31mBanner \e[33mNombre\e[32m : "
+read varbanner
+echo -e -n "\e[1m\e[33m\nCual es tu palabra clave Nombre\e[32m : "
+read varcowsay
+echo  "cowsay -f eyes $varcowsay | lolcat" > Cowsay.txt
+echo "toilet -f big '    $varbanner' -F gay | lolcat" > Banner.txt
+echo
+#CARPETA RAIZ
+echo "clear" > Clear.txt
+cat "Clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Cowsay.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "Banner.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+#MENSAJE DE FINAL
+clear
+    sleep 2.0
+echo "GRACIAS POR USAR MI SCRIP TD-BANNER"
+echo "CREDITOS: @HackingPCH"
+   sleep 1.0
+echo ""
+echo -e "$rojo ESTE SCRIPT ESTA ECHO PARA USTEDES MIS $verde SEGUIDORES"
+echo -e "$rojo CREDITOS: $rojo @HackingPCH"
+echo -e "$verde YouTube: $rojo @HackinPCH"
+   sleep 1.0
+echo -e "\e[0;33m"
+   figlet like
+   sleep 1.0
+echo -e "\e[0;34m"
+   figlet suscribete
+echo ""
+echo -e "$rojo--------------------"
+   sleep 1.0
+echo "---=T============---"
+   sleep 1.0
+echo "---==H===========---"
+   sleep 1.0
+echo "---===O==========---"
+   sleep 1.0
+echo "---====N=========---"
+   sleep 1.0
+echo "---=====Y========---"
+   sleep 1.0
+echo "---======D=======---"
+   sleep 1.0
+echo "---=======R======---"
+   sleep 1.0
+echo "---=========O====---"
+   sleep 1.0
+echo "---==========I===---"
+   sleep 1.0
+echo "---===========D==---"
+   sleep 1.0
+echo -e "$rojo--------------------"
+   sleep 3.0
+clear
+echo -e "\e[1m\e[31mFAVOR \e[1m\e[32mDE \e[1m\e[31mABRIR \e[1m\e[32mOTRA \e[1m\e[33mVENTANA"
+echo -e "$azulcielo"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+echo "<========"
+   sleep 1.0
+#MUNECO
+echo -e "$rojo"
+echo "----------"
+   sleep 0.5
+echo "    --    "
+   sleep 0.5
+echo "   ----   "
+   sleep 0.5
+echo "_/<[°°]>\_"
+   sleep 0.5
+echo "   [  ]   "
+   sleep 0.5
+echo "   //\\\  "
+   sleep 0.5
+echo "  //  \\\ "
+   sleep 0.5
+echo "----------"
+echo ""
